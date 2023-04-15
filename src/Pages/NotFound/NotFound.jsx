4ef2bab3320c63e1c2796a74ecb6eaceff2f14f5
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-04-15 15:30:39 +0600

    page created

---
+++ src/Pages/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1> 404 || Not Found the page</h1>
+        </div>
+    );
+};
+
+export default NotFound;
