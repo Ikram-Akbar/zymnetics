e40b585d301bf4d74606437cc01dc476fa8f44de
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-04-15 17:00:44 +0600

    not found page added with routin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Service from './Pages/Service/Service';
 import Main from './Layout/Main';
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   const router = createBrowserRouter([
@@ -12,12 +13,14 @@ function App() {
       element: <Main />,
       children:
         [
-          { path: "/", element: <Home />, },
-          { path: "home", element: <Home />, },
-          { path: "about", element: <About />, },
-          { path: "service", element: <Service />, }
+          { path: "/", element: <Home /> },
+          { path: "home", element: <Home /> },
+          { path: "about", element: <About /> },
+          { path: "service", element: <Service /> },
+          { path: '*', element: <NotFound /> }
         ]
     },
+
 
 
 
