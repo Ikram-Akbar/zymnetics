f7ae782d52cbef675869fc23525a048495cfbaae
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-04-15 16:00:30 +0600

    routing added

--- src/Pages/Service/Service.jsx
+++ src/Pages/Service/Service.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../../Header/Header';
 
 const Service = () => {
     return (
         <div>
+            <Header />
             <h1>this is service page</h1>
         </div>
     );
