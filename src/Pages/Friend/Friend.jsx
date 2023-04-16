6bfb82c542f2d6c9e29db33a55a6f16cb3828b03
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-04-16 10:55:54 +0600

    dynamic user load

--- src/Pages/Friend/Friend.jsx
+++ src/Pages/Friend/Friend.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Friend = ({ friend }) => {
-  const { name, username, email } = friend;
+  const { id, name, username, email } = friend;
   return (
     <div>
-      <h4>name : {name} </h4>
+      <h4>Name : {name} </h4>
+      <p>Email: {email} </p>
+      <p>
+        <Link to={`/friend/${id}`}>{username}</Link>
+      </p>
     </div>
   );
 };
