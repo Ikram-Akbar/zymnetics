260a7d305f8d7f48d153fd7c191a831b1aa71e54
Ikram-Git-Coder <ikramakbar71@gmail.com>
2023-04-16 10:37:05 +0600

    load data using userLoad hook

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Service from './Pages/Service/Service';
 import Main from './Layout/Main';
+import Friends from './Pages/Friends/Friends.jsx'
 import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
@@ -17,6 +18,13 @@ function App() {
           { path: "home", element: <Home /> },
           { path: "about", element: <About /> },
           { path: "service", element: <Service /> },
+          {
+            path: "friends",
+            element: <Friends />,
+            loader: async () => { 
+              return fetch("https://jsonplaceholder.typicode.com/users")
+            },
+          },
           { path: '*', element: <NotFound /> }
         ]
     },
